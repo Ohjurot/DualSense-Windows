780cbf8ec36eff2ccb92775ffdd1ce6793854698
Laggamer2005 <158077584+Laggamer2005@users.noreply.github.com>
2024-12-07 11:20:59 +0000

    Update DSW_Api.h

--- VS19_Solution/DualSenseWindows/include/DualSenseWindows/DSW_Api.h
+++ VS19_Solution/DualSenseWindows/include/DualSenseWindows/DSW_Api.h
@@ -1,84 +1,82 @@
 /*
-	DSW_Api.h is part of DualSenseWindows
-	https://github.com/Ohjurot/DualSense-Windows
+    DSW_Api.h is part of DualSenseWindows
+    https://github.com/Ohjurot/DualSense-Windows
 
-	Contributors of this file:
-	11.2020 Ludwig Füchsl
+    Contributors of this file:
+    11.2020 Ludwig FÃ¼chsl
 
-	Licensed under the MIT License (To be found in repository root directory)
+    Licensed under the MIT License (To be found in repository root directory)
 */
 #pragma once
 
 #if defined(DS5W_BUILD_DLL)
-#define DS5W_API __declspec(dllexport)
-#elif defined(DS5W_BUILD_LIB)
-#define DS5W_API
-#elif defined(DS5W_USE_LIB)
-#define DS5W_API
+    #define DS5W_API __declspec(dllexport)
+#elif defined(DS5W_BUILD_LIB) || defined(DS5W_USE_LIB)
+    #define DS5W_API
 #else
-#define DS5W_API __declspec(dllimport)
+    #define DS5W_API __declspec(dllimport)
 #endif
 
-#define DS5W_SUCCESS(expr) ((expr) == _DS5W_ReturnValue::OK)
-#define DS5W_FAILED(expr) ((expr) != _DS5W_ReturnValue::OK)
+#define DS5W_SUCCESS(expr) ((expr) == DS5W_ReturnValue::OK)
+#define DS5W_FAILED(expr) ((expr) != DS5W_ReturnValue::OK)
 
-#define DS5W_OK _DS5W_ReturnValue::OK
-#define DS5W_E_UNKNOWN _DS5W_ReturnValue::E_UNKNOWN
-#define DS5W_E_INSUFFICIENT_BUFFER _DS5W_ReturnValue::E_INSUFFICIENT_BUFFER
-#define DS5W_E_EXTERNAL_WINAPI _DS5W_ReturnValue::E_EXTERNAL_WINAPI
-#define DS5W_E_STACK_OVERFLOW _DS5W_ReturnValue::E_STACK_OVERFLOW
-#define DS5W_E_INVALID_ARGS _DS5W_ReturnValue::E_INVALID_ARGS
-#define DS5W_E_CURRENTLY_NOT_SUPPORTED _DS5W_ReturnValue::E_CURRENTLY_NOT_SUPPORTED
-#define DS5W_E_DEVICE_REMOVED _DS5W_ReturnValue::E_DEVICE_REMOVED
-#define DS5W_E_BT_COM _DS5W_ReturnValue::E_BT_COM
+#define DS5W_OK DS5W_ReturnValue::OK
+#define DS5W_E_UNKNOWN DS5W_ReturnValue::E_UNKNOWN
+#define DS5W_E_INSUFFICIENT_BUFFER DS5W_ReturnValue::E_INSUFFICIENT_BUFFER
+#define DS5W_E_EXTERNAL_WINAPI DS5W_ReturnValue::E_EXTERNAL_WINAPI
+#define DS5W_E_STACK_OVERFLOW DS5W_ReturnValue::E_STACK_OVERFLOW
+#define DS5W_E_INVALID_ARGS DS5W_ReturnValue::E_INVALID_ARGS
+#define DS5W_E_CURRENTLY_NOT_SUPPORTED DS5W_ReturnValue::E_CURRENTLY_NOT_SUPPORTED
+#define DS5W_E_DEVICE_REMOVED DS5W_ReturnValue::E_DEVICE_REMOVED
+#define DS5W_E_BT_COM DS5W_ReturnValue::E_BT_COM
 
 /// <summary>
 /// Enum for return values
 /// </summary>
-typedef enum class _DS5W_ReturnValue : unsigned int {
-	/// <summary>
-	/// Operation completed without an error
-	/// </summary>
-	OK = 0,
+typedef enum class DS5W_ReturnValue : unsigned int {
+    /// <summary>
+    /// Operation completed without an error
+    /// </summary>
+    OK = 0,
 
-	/// <summary>
-	/// Operation encountered an unknown error
-	/// </summary>
-	E_UNKNOWN = 1,
+    /// <summary>
+    /// Operation encountered an unknown error
+    /// </summary>
+    E_UNKNOWN = 1,
 
-	/// <summary>
-	/// The user supplied buffer is to small
-	/// </summary>
-	E_INSUFFICIENT_BUFFER = 2,
+    /// <summary>
+    /// The user supplied buffer is too small
+    /// </summary>
+    E_INSUFFICIENT_BUFFER = 2,
 
-	/// <summary>
-	/// External unexpected winapi error (please report as issue if you get this error!)
-	/// </summary>
-	E_EXTERNAL_WINAPI = 3,
+    /// <summary>
+    /// External unexpected WinAPI error (please report as issue if you get this error!)
+    /// </summary>
+    E_EXTERNAL_WINAPI = 3,
 
-	/// <summary>
-	/// Not enought memroy on the stack
-	/// </summary>
-	E_STACK_OVERFLOW = 4,
+    /// <summary>
+    /// Not enough memory on the stack
+    /// </summary>
+    E_STACK_OVERFLOW = 4,
 
-	/// <summary>
-	/// Invalid arguments
-	/// </summary>
-	E_INVALID_ARGS = 5,
+    /// <summary>
+    /// Invalid arguments
+    /// </summary>
+    E_INVALID_ARGS = 5,
 
-	/// <summary>
-	/// This feature is currently not supported
-	/// </summary>
-	E_CURRENTLY_NOT_SUPPORTED = 6,
+    /// <summary>
+    /// This feature is currently not supported
+    /// </summary>
+    E_CURRENTLY_NOT_SUPPORTED = 6,
 
-	/// <summary>
-	/// Device was disconnected
-	/// </summary>
-	E_DEVICE_REMOVED = 7,
+    /// <summary>
+    /// Device was disconnected
+    /// </summary>
+    E_DEVICE_REMOVED = 7,
 
-	/// <summary>
-	/// Bluetooth communication error
-	/// </summary>
-	E_BT_COM = 8,
+    /// <summary>
+    /// Bluetooth communication error
+    /// </summary>
+    E_BT_COM = 8,
 
-} DS5W_ReturnValue, DS5W_RV;
+} DS5W_RV;
